7fb91a3e5073596a85fbe457f8bedbcf814e13d4
Leszek Tejwan <leszek.tejwan@gmail.com>
2025-12-31 17:34:26 +0100

    SEO Optimize 3

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./en/*.html",
+    "./pl/*.html",
+    "./assets/**/*.{js,html}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
